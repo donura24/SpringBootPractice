57ea46be22717fcdef1427695c311c188de0f268
donura24 <teodorpenchev24@gmail.com>
2023-03-13 22:18:13 +0200

    commit

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
@@ -22,6 +24,16 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentsByEmail(student.getEmail());
+        if (studentOptional.isPresent()) throw new IllegalStateException("email token");
+        studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) throw new IllegalStateException("Sudent with ID "+studentId+" does not exists.");
+
+        studentRepository.deleteById(studentId);
+    }
+
 }
