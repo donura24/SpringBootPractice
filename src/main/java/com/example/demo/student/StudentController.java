ec178c91fc8c944b1a61e9bd8b404de6c2bbadd9
donura24 <teodorpenchev24@gmail.com>
2023-03-13 21:59:04 +0200

    commit

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,13 +9,20 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
     private final StudentService studentService;
+    private Object Student;
 
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
+
     @GetMapping
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return studentService.getStudent();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
     }
 }
